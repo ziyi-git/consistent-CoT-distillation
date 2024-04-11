42270ea5744c9e40e0ab05b90008e4a336e1bb6f
root <lc_lc2000@hotmail.com>
2024-04-11 03:35:13 +0000

    不重要的更新。

--- scripts/run_contrastive_decoding.sh
+++ scripts/run_contrastive_decoding.sh
@@ -4,13 +4,14 @@ dataset="strategyQA"
 prompt="explanation"
 
 output_prefix="outputs/${dataset}/"
+if [ -d "$output_prefix" ]; then
+    rm -r $output_prefix
+fi
 mkdir -p $output_prefix
 
 python -u \
     contrastive_decoding_rationalization.py \
     --output_prefix $output_prefix \
     --dataset $dataset \
-    --prompt $prompt
-    # --eval_split dev,train,train.counterfactual \
-    # --debug
-    # >${output_prefix}/rationalization.log 2>&1 &
+    --prompt $prompt \
+    >${output_prefix}/rationalization.log 2>&1 &
